1ef2699dc82e5c3c5b2aa6720840223f5cf66aa3
Tiago Martinez <tiagomartinez334@hotmail.com>
2026-06-30 19:49:42 -0300

    deployment start

--- CineTup.Presentation/Controllers/MovieControllers.cs
+++ CineTup.Presentation/Controllers/MovieControllers.cs
@@ -7,7 +7,6 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace CineTup.Presentation.Controllers
 {
-    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class MovieController : ControllerBase
--- CineTup.Presentation/Program.cs
+++ CineTup.Presentation/Program.cs
@@ -97,6 +97,8 @@ if (app.Environment.IsDevelopment())
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 app.UseHttpsRedirection();
+app.UseCors(x  => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
+
 app.UseAuthentication();
 app.UseAuthorization();
 
